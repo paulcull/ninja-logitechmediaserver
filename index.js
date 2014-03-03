0871f504452582d54a0c2f675c4c56e2d6edc9b3
Paul Cullender <paul@pkhome.co.uk>
2014-03-03 09:11:04 +0000

    fix player links

--- index.js
+++ index.js
@@ -256,7 +256,7 @@ function LMSDevice(opts, app, player, mac) {
       //if (player.id !== self.devices.mediaObject.mac) { self.app.log.debug('(Squeezebox) : Event: %s skipped for %s' ,eventName, player.id);return };
       self.app.log.debug('(Squeezebox) : Event: %s - Got Player %s and sending to %s with value %s', eventName, player.id, self.devices.mediaObject.mac, e);
       self.app.log.debug('(Squeezebox) : Got %s in ninja',eventName);
-      if (self.devices.mediaObject._data.track.source === 'radio') {
+      if (self.devices.mediaObject._data.track.source === 'radio'||self.devices.mediaObject._data.track.source === 'spotify') {
         self.devices.mediaObject._data.track.name = e;
         self.app.log.debug('(Squeezebox) : about to send media object for %s...',opts.lmsname);
         // uncomment to see the media object being sent
@@ -276,6 +276,22 @@ function LMSDevice(opts, app, player, mac) {
       //self.app.log.debug('(Squeezebox) : Got %s in ninja',eventName);
       //self.app.log.debug('(Squeezebox) : Using these options: %s',JSON.stringify(opts));
       self.app.log.debug('(Squeezebox) : spotify URL is %s',spotify_host+spotify_url_start+e.id);
+      //
+      self.devices.mediaObject._data.track.artist = 'Spotify';
+      self.devices.mediaObject._data.track.album = '';
+      self.devices.mediaObject._data.track.disc_number = '';
+      self.devices.mediaObject._data.track.track_number = '';
+      self.devices.mediaObject._data.track.duration = '';
+      self.devices.mediaObject._data.track.id = e.id;
+      self.devices.mediaObject._data.state.track_id = e.id;
+      self.devices.mediaObject._data.track.album_artist = e.artist;
+      self.devices.mediaObject._data.track.squeeze_url = '';
+      self.devices.mediaObject._data.track.source = e.source;
+      self.devices.mediaObject._data.track.spotify_url = spotify_host+spotify_url_start+e.id;
+      //
+      self.devices.mediaObject._data.track.name = '';  
+      self.devices.mediaObject._data.image = '';
+
       //Have to go out to spotify to get the track details
       var get_options = {
         hostname: spotify_host,
@@ -283,70 +299,42 @@ function LMSDevice(opts, app, player, mac) {
         method: 'GET'
       };
 
+      var req = https.get(get_options, function(res) {
+        console.log("Got response: " + res.statusCode);
+        // console.log("Got headers: " + JSON.stringify(res.headers));
+        // console.log(res);
+        res.on('data',function(chunk) {
+          console.log('===== ALL CHUNK DATA');
+          console.log(chunk);
+          var _spotData = JSON.parse(chunk);
+          console.log('===== ALL SPOT DATA');
+          console.log(JSON.stringify(_spotData));
+          // //set track details for spotify - not many
+          self.devices.mediaObject._data.track.name = _spotData.title;
+          self.devices.mediaObject._data.image = _spotData.thumbnail_url;
 
-      // var req2 = request('https://'+spotify_host+spotify_url_start+e.id, function (error, response, body) {
-      //   if (!error && response.statusCode == 200) {
-      //     console.log('-=-=RRRR-=-=-'+response) // Print the google web page.
-      //     console.log('-=-=BBBB-=-=-'+body) // Print the google web page.
-      //   }
-      // });
-
-      // var req = https.request(get_options, function(res) {
-      //   console.log("statusCode: ", res.statusCode);
-      //   console.log("headers: ", res.headers);
-
-      //   res.on('data', function(d) {
-      //     console.log('***** DATA**** '+d);
-      //     //process.stdout.write(d);
-      //   });
-      // });
-      // req.end();
-
-      // req.on('error', function(e) {
-      //   console.error(e);
-      // });
+          self.app.log.debug('(Squeezebox) : about to send media object for %s...',opts.lmsname);
+          //uncomment to see the media object being sent
+          self.app.log.debug('(Squeezebox) : %s (spotify data): object : %s',eventName, JSON.stringify(self.devices.mediaObject._data));
+          self.devices.mediaObject.emit('data',self.devices.mediaObject._data)
 
 
-      // var req = https.get(get_options, function(res) {
-      //   // console.log("Got response: " + res.statusCode);
-      //   // console.log("Got headers: " + JSON.stringify(res.headers));
-      //   // console.log(res);
-      //   res.on('data',function(chunk) {
-      //     console.log('===== ALL CHUNK DATA');
-      //     console.log(chunk);
-      //     var _spotData = JSON.parse(chunk);
-      //     console.log('===== ALL SPOT DATA');
-      //     console.log(JSON.stringify(_spotData));
-      //     //set track details for spotify - not many
-      //     self.devices.mediaObject._data.track.name = _spotData.title;
-      //     self.devices.mediaObject._data.track.artist = '';
-      //     self.devices.mediaObject._data.track.album = '';
-      //     self.devices.mediaObject._data.track.disc_number = '';
-      //     self.devices.mediaObject._data.track.track_number = '';
-      //     self.devices.mediaObject._data.track.duration = '';
-      //     self.devices.mediaObject._data.track.id = e.id;
-      //     self.devices.mediaObject._data.state.track_id = e.id;
-      //     self.devices.mediaObject._data.track.album_artist = e.artist;
-      //     self.devices.mediaObject._data.track.squeeze_url = '';
-      //     self.devices.mediaObject._data.track.source = e.source;
-      //     self.devices.mediaObject._data.track.spotify_url = spotify_host+spotify_url_start+e.id;
-      //     self.devices.mediaObject._data.image = _spotData.thumbnail_url;
+        });
+         res.on('error', function(e) {console.log(e)});
+         res.on('close', function(e) {console.log('res close received')});
+         res.on('end', function(a,b,c) {console.log('res end received:  '+a,b,c)});
+      });
+      req.end();
 
-      //     self.app.log.debug('(Squeezebox) : about to send media object for %s...',opts.lmsname);
-      //     //uncomment to see the media object being sent
-      //     self.app.log.debug('(Squeezebox) : %s : object : %s',eventName, JSON.stringify(self.devices.mediaObject._data));
-      //     self.devices.mediaObject.emit('data',self.devices.mediaObject._data)
+      req.on('error', function(e) {
+        self.app.log.error('(Squeezebox) : Got error: ' + e.message);
+      });
 
-      //   });
-      //    res.on('error', function(e) {console.log(e)});
-      //    res.on('close', function(e) {console.log('res close received')});
-      //    res.on('end', function(e) {console.log('res end received:  '+e)});
-      // });
-      // req.end();
+      self.app.log.debug('(Squeezebox) : about to send media object for %s...',opts.lmsname);
+      //uncomment to see the media object being sent
+      self.app.log.debug('(Squeezebox) : %s : object : %s',eventName, JSON.stringify(self.devices.mediaObject._data));
+      self.devices.mediaObject.emit('data',self.devices.mediaObject._data)
 
-      // req.on('error', function(e) {
-      //   self.app.log.error('(Squeezebox) : Got error: ' + e.message);
-      // });
 
     });
   });
@@ -397,99 +385,100 @@ function LMSDevice(opts, app, player, mac) {
     });
   });
 
-  // // this is the playing mode
-  // 'mode'
-  // .split(',').forEach(  function listenToNotification(eventName) {
-  //   player.on(eventName, function(e) {
-  //     //if (player.id !== self.devices.mediaObject.mac) { return };
-  //     self.app.log.debug('(Squeezebox) : Event: %s - Got Player %s and sending to %s with value %s', eventName, player.id, self.devices.mediaObject.mac, e);
-  //     console.log('Event: %s - Got Player %s and sending to %s with value %s', eventName, player.id, self.devices.mediaObject.mac, e);
-  //     //self.app.log.debug('**** nb emit logitech path for %s with value %s',eventName,JSON.stringify(e));
-  //     self.app.log.debug('(Squeezebox) : Got Event %s with mode as %s',eventName,e);
-  //     self.devices.mediaObject._data.state.mode = e
-  //     if (e === 'play') {
-  //       self.devices.mediaObject._data.state.nextmode = 'Pause'
-  //       if (self.devices.mediaObject._data.state.state === 'off') {
-  //         self.devices.mediaObject._data.state.nextmode = '-'
-  //       } 
-  //     } else {
-  //       self.devices.mediaObject._data.state.nextmode = 'Play'          
-  //     }
-  //     self.app.log.debug('(Squeezebox) : about to send media object for %s...',opts.lmsname);
-  //     self.app.log.debug('(Squeezebox) : %s - %s : object : %s',eventName,e, JSON.stringify(self.devices.mediaObject._data));
+  // this is the playing mode
+  'mode'
+  .split(',').forEach(  function listenToNotification(eventName) {
+    player.on(eventName, function(e) {
+      //if (player.id !== self.devices.mediaObject.mac) { self.app.log.debug('(Squeezebox) : Event: %s skipped for %s' ,eventName, player.id);return };
+      self.app.log.debug('(Squeezebox) : Event: %s - Got Player %s and sending to %s with value %s', eventName, player.id, self.devices.mediaObject.mac, e);
+      console.log('Event: %s - Got Player %s and sending to %s with value %s', eventName, player.id, self.devices.mediaObject.mac, e);
+      //self.app.log.debug('**** nb emit logitech path for %s with value %s',eventName,JSON.stringify(e));
+      self.app.log.debug('(Squeezebox) : Got Event %s with mode as %s',eventName,e);
+      self.devices.mediaObject._data.state.mode = e
+      if (e === 'play') {
+        self.devices.mediaObject._data.state.nextmode = 'Pause'
+        if (self.devices.mediaObject._data.state.state === 'off') {
+          self.devices.mediaObject._data.state.nextmode = '-'
+        } 
+      } else {
+        self.devices.mediaObject._data.state.nextmode = 'Play'          
+      }
+      self.app.log.debug('(Squeezebox) : about to send media object for %s...',opts.lmsname);
+      self.app.log.debug('(Squeezebox) : %s - %s : object : %s',eventName,e, JSON.stringify(self.devices.mediaObject._data));
 
-  //     self.devices.mediaObject.emit('data',self.devices.mediaObject._data);
-  //     //player.getSongInfo('file:'+e.file);
-  //   });
-  // });
+      self.devices.mediaObject.emit('data',self.devices.mediaObject._data);
+      //player.getSongInfo('file:'+e.file);
+    });
+  });
 
-  // // does what it says
-  // 'volume'
-  // .split(',').forEach(  function listenToNotification(eventName) {
-  //   player.on(eventName, function(e) {
-  //     //if (player.id !== self.devices.mediaObject.mac) { return };
-  //     self.app.log.debug('(Squeezebox) : Event: %s - Got Player %s and sending to %s with value %s', eventName, player.id, self.devices.mediaObject.mac, e);
-  //     self.devices.mediaObject._data.state.volume = player.getNoiseLevel();
-  //     self.app.log.debug('(Squeezebox) : about to send media object for %s...',opts.lmsname);      
-  //     self.app.log.debug('(Squeezebox) : %s : object : %s',eventName, JSON.stringify(self.devices.mediaObject._data));
-  //     self.devices.mediaObject.emit('data',self.devices.mediaObject._data);
-  //     self.app.log.debug('(Squeezebox) : Volumes is %s',self.devices.mediaObject._data.state.volume);
-  //     //self.devices.mediaObject.write('data',self.devices.mediaObject._data)
-  //   });
-  // });
+  // does what it says
+  'volume'
+  .split(',').forEach(  function listenToNotification(eventName) {
+    player.on(eventName, function(e) {
+      //if (player.id !== self.devices.mediaObject.mac) { self.app.log.debug('(Squeezebox) : Event: %s skipped for %s' ,eventName, player.id);return };
+      self.app.log.debug('(Squeezebox) : Event: %s - Got Player %s and sending to %s with value %s', eventName, player.id, self.devices.mediaObject.mac, e);
+      self.devices.mediaObject._data.state.volume = player.getNoiseLevel();
+      if (self.devices.mediaObject._data.state.state === 'off') {player.getPower();}
+      self.app.log.debug('(Squeezebox) : about to send media object for %s...',opts.lmsname);      
+      self.app.log.debug('(Squeezebox) : %s : object : %s',eventName, JSON.stringify(self.devices.mediaObject._data));
+      self.devices.mediaObject.emit('data',self.devices.mediaObject._data);
+      self.app.log.debug('(Squeezebox) : Volumes is %s',self.devices.mediaObject._data.state.volume);
+      //self.devices.mediaObject.write('data',self.devices.mediaObject._data)
+    });
+  });
 
-  // // set subscriptions to each of the events
-  // 'power'//State,power'
-  // .split(',').forEach(  function listenToNotification(eventName) {
-  //   player.on(eventName, function(e) {
-  //     //if (player.id !== self.devices.mediaObject.mac) { return };
-  //     self.app.log.debug('(Squeezebox) : Event: %s - Got Player %s and sending to %s with value %s', eventName, player.id, self.devices.mediaObject.mac, e);
-  //     //console.log('e for %s is this: %s',eventName, e);
-  //     var _state = e==1 ? 'on' : 'off';
-  //     //console.log('old is %s : new is %s',e,_state);
-  //     self.app.log.debug('(Squeezebox) : Power old is %s : new is %s',e,_state);
-  //     // if switched off - reset everything
-  //     if (!e) {
-  //       console.log('switching off...');
-  //       self.devices.mediaObject._data.state.track_id = null;
-  //       self.devices.mediaObject._data.state.volume = 0;
-  //       self.devices.mediaObject._data.state.position = null;
-  //       self.devices.mediaObject._data.state.state = "off";
-  //       self.devices.mediaObject._data.state.nextstate = "On";
-  //       self.devices.mediaObject._data.track.duration = 0;
-  //       self.devices.mediaObject._data.state.mode = "off"
-  //       self.devices.mediaObject._data.state.nextmode = "-"
+  // set subscriptions to each of the events
+  'power'//State,power'
+  .split(',').forEach(  function listenToNotification(eventName) {
+    player.on(eventName, function(e) {
+      //if (player.id !== self.devices.mediaObject.mac) { self.app.log.debug('(Squeezebox) : Event: %s skipped for %s' ,eventName, player.id);return };
+      self.app.log.debug('(Squeezebox) : Event: %s - Got Player %s and sending to %s with value %s', eventName, player.id, self.devices.mediaObject.mac, e);
+      //console.log('e for %s is this: %s',eventName, e);
+      var _state = e==1 ? 'on' : 'off';
+      //console.log('old is %s : new is %s',e,_state);
+      self.app.log.debug('(Squeezebox) : Power old is %s : new is %s',e,_state);
+      // if switched off - reset everything
+      if (!e) {
+        console.log('switching off...');
+        self.devices.mediaObject._data.state.track_id = null;
+        self.devices.mediaObject._data.state.volume = 0;
+        self.devices.mediaObject._data.state.position = null;
+        self.devices.mediaObject._data.state.state = "off";
+        self.devices.mediaObject._data.state.nextstate = "On";
+        self.devices.mediaObject._data.track.duration = 0;
+        self.devices.mediaObject._data.state.mode = "off"
+        self.devices.mediaObject._data.state.nextmode = "-"
 
-  //       self.devices.mediaObject._data.track.name = 'Player is off';
-  //       self.devices.mediaObject._data.track.artist = '';
-  //       self.devices.mediaObject._data.track.album = '';
-  //       self.devices.mediaObject._data.track.disc_number = '';
-  //       self.devices.mediaObject._data.track.track_number = '';
-  //       self.devices.mediaObject._data.track.id = '';
-  //       self.devices.mediaObject._data.state.track_id = '';
-  //       self.devices.mediaObject._data.track.album_artist = null;
-  //       self.devices.mediaObject._data.track.squeeze_url = null;
-  //       self.devices.mediaObject._data.track.spotify_url = null;
-  //       self.devices.mediaObject._data.track.source = null;
+        self.devices.mediaObject._data.track.name = 'Player is off';
+        self.devices.mediaObject._data.track.artist = '';
+        self.devices.mediaObject._data.track.album = '';
+        self.devices.mediaObject._data.track.disc_number = '';
+        self.devices.mediaObject._data.track.track_number = '';
+        self.devices.mediaObject._data.track.id = '';
+        self.devices.mediaObject._data.state.track_id = '';
+        self.devices.mediaObject._data.track.album_artist = null;
+        self.devices.mediaObject._data.track.squeeze_url = null;
+        self.devices.mediaObject._data.track.spotify_url = null;
+        self.devices.mediaObject._data.track.source = null;
 
-  //       self.devices.mediaObject._data.image = 'null';
+        self.devices.mediaObject._data.image = 'null';
 
-  //       self.app.log.debug('(Squeezebox) : about to send media object for %s...',opts.lmsname);
-  //       self.app.log.debug('(Squeezebox) : %s : object : %s',eventName, JSON.stringify(self.devices.mediaObject._data));
-  //       self.devices.mediaObject.emit('data',self.devices.mediaObject._data);
+        self.app.log.debug('(Squeezebox) : about to send media object for %s...',opts.lmsname);
+        self.app.log.debug('(Squeezebox) : %s : object : %s',eventName, JSON.stringify(self.devices.mediaObject._data));
+        self.devices.mediaObject.emit('data',self.devices.mediaObject._data);
 
-  //       //
-  //      } else {
-  //       self.devices.mediaObject._data.state.state = 'on';
-  //       self.devices.mediaObject._data.state.nextstate = 'Off';        
-  //       self.devices.mediaObject._data.state.volume = player.getNoiseLevel()
-  //      player.getPlayerSong();
-
-  //      }
-  //     //console.log('about to send media object with %s',self.devices.mediaObject._data);
-  //     //self.devices.powerState.emit('data',_state);
-  //   });
-  // });
+        //
+       } else {
+        self.devices.mediaObject._data.state.state = 'on';
+        if (self.devices.mediaObject._data.track.name === 'Player is off') {self.devices.mediaObject._data.track.name = '';};
+        self.devices.mediaObject._data.state.nextstate = 'Off';        
+        self.devices.mediaObject._data.state.volume = player.getNoiseLevel()
+        player.getPlayerSong();
+       }
+      //console.log('about to send media object with %s',self.devices.mediaObject._data);
+      //self.devices.powerState.emit('data',_state);
+    });
+  });
 
 
   // these the device types
